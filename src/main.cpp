6f1a26af3e95d581172806a6ffa78b5fb6141da7
Casey Primel <casey.primel@gmail.com>
2020-12-24 21:34:59 -0500

    Added write_color method, added missing method to vec3 class, integrated color class to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include "vec3.h"
+#include "color.h"
 
 int main() {
     // Image
@@ -6,21 +7,13 @@ int main() {
     const int image_height = 256;
 
     // Render
-
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int i = image_height - 1; i >= 0; --i) {
         std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
         for (int j = 0; j < image_width; ++j) {
-            auto r = double(j) / (image_width - 1);
-            auto g = double(i) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(double(j)/(image_width-1), double(i)/(image_height-1),0.25);
+            write_color(std::cout, pixel_color);
         }
     }
     std::cerr << "\nDone.\n";
